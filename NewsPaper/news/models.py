022ce7aea196b9319f885c4681fcdb5d55607b0d
Ivan <fedechkin.ivan@mail.ru>
2022-07-05 12:21:15 +0300

    django2

---
+++ NewsPaper/news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Order(models.Model):
+    pass
+
+class Product(models.Model):
+    pass
+
+class Staff(models.Model):
+    pass
+
+class ProductOrder(models.Model):
+    pass
+
+
