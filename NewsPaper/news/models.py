83504d58f2f8c3821520385e0c5ff253cc0ff401
Ivan <fedechkin.ivan@mail.ru>
2022-07-08 13:55:24 +0300

    django2

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,15 +1,57 @@
 from django.db import models
+from datetime import datetime
+from news.slovar import *
 
 class Order(models.Model):
-    pass
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
+    products = models.ManyToManyField('Product', through='ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:
+            return (datetime.now() - self.time_in).total_seconds() // 60
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+    product_1 = Product(name='Витая пара(5м)', price=993.0)
+    product_1.save()
+
+    product_2 = Product.odjects.create(name='клавиатура', price=1060.0)
 
 class Staff(models.Model):
-    pass
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
+    labor_contract = models.IntegerField()
+
+    def get_last_name(self):
+        return self.full_name.split()[0]
 
 class ProductOrder(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    in_order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    _amount = models.IntegerField(default=1, db_column='amount')
 
+    def product_sum(self):
+        return self.product.price * self.amount
 
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
